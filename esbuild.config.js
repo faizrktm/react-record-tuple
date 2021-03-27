91c23f9a08ea031fbd5209a0e8c16a769c121923
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-03-27 18:31:49 +0700

    fix: generate html after dist folder created

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,11 +2,6 @@ import esbuild from 'esbuild';
 import babel from 'esbuild-plugin-babel';
 import fs from 'fs';
 
-const htmlContent = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>React Tuple and Record</title></head><body><div id="root"></div> <script src="./index.js" defer></script> </body></html>';
-fs.writeFile(process.cwd() + '/dist/index.html', htmlContent, 'utf8', (error) => {
-  console.error(error);
-});
-
 esbuild
     .build({
         entryPoints: ['src/index.jsx'],
@@ -18,5 +13,11 @@ esbuild
         define: {
           'process.env.NODE_ENV': '"production"',
         }
+    })
+    .then(() => {
+      const htmlContent = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>React Tuple and Record</title></head><body><div id="root"></div> <script src="./index.js" defer></script> </body></html>';
+      fs.writeFile(process.cwd() + '/dist/index.html', htmlContent, 'utf8', (error) => {
+        console.error(error);
+      });
     })
     .catch(() => process.exit(1));
