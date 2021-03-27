88ded3ef3cbcc40af677d2756257f06b580377fc
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-03-27 18:16:19 +0700

    add html template

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,11 @@
 import esbuild from 'esbuild';
 import babel from 'esbuild-plugin-babel';
+import fs from 'fs';
+
+const htmlContent = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>React Tuple and Record</title></head><body><div id="root"></div> <script src="./index.js" defer></script> </body></html>';
+fs.writeFile('dist/index.html', htmlContent, (error) => {
+  console.error(error);
+});
 
 esbuild
     .build({
