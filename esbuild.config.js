ffe2c915ef4bd145f6d4d2a9cd96166bb7686fbd
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-03-27 18:28:16 +0700

    fix: write html

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,7 +3,7 @@ import babel from 'esbuild-plugin-babel';
 import fs from 'fs';
 
 const htmlContent = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>React Tuple and Record</title></head><body><div id="root"></div> <script src="./index.js" defer></script> </body></html>';
-fs.writeFile('dist/index.html', htmlContent, (error) => {
+fs.writeFile(process.cwd() + '/dist/index.html', htmlContent, 'utf8', (error) => {
   console.error(error);
 });
 
